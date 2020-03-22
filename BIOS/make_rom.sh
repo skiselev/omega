bf2a86727fede50179a65924a1064bd05a3034f0
Sergey Kiselev <skiselev@gmail.com>
2020-03-21 22:38:59 -0700

    BIOS: Add creating MSX2+ BIOS images. Use OpenMSX file names for BIOS ROMs
    Signed-off-by: Sergey Kiselev <skiselev@gmail.com>

--- BIOS/make_rom.sh
+++ BIOS/make_rom.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-MSX2_NTSC_ROMS_DIR=sanyo-phc-23
-MSX2_PAL_ROMS_DIR=cx7m128
+SYSTEM_ROMS_DIR=systemroms
 CBIOS_ROMS_DIR=cbios-0.29a/roms
 
 # copy MSX2 NTSC ROMs
-cat ${MSX2_NTSC_ROMS_DIR}/23bios.rom > omega_msx2_ntsc.bin
+cat ${SYSTEM_ROMS_DIR}/phc-23jb_basic-bios2.rom > omega_msx2_ntsc.bin
 dd if=/dev/zero ibs=1k count=32 | tr "\000" "\377" >> omega_msx2_ntsc.bin
-cat ${MSX2_NTSC_ROMS_DIR}/23ext.rom >> omega_msx2_ntsc.bin
+cat ${SYSTEM_ROMS_DIR}/phc-23jb_msx2sub.rom >> omega_msx2_ntsc.bin
 dd if=/dev/zero ibs=1k count=176 | tr "\000" "\377" >> omega_msx2_ntsc.bin
 
 # copy C-BIOS NTSC ROMs
@@ -18,9 +17,9 @@ cat ${CBIOS_ROMS_DIR}/cbios_sub.rom >> omega_msx2_ntsc.bin
 dd if=/dev/zero ibs=1k count=176 | tr "\000" "\377" >> omega_msx2_ntsc.bin
 
 # copy MSX2 PAL ROMs
-cat ${MSX2_PAL_ROMS_DIR}/cx7mbios.rom > omega_msx2_pal.bin
+cat ${SYSTEM_ROMS_DIR}/nms8250_basic-bios2.rom > omega_msx2_pal.bin
 dd if=/dev/zero ibs=1k count=32 | tr "\000" "\377" >> omega_msx2_pal.bin
-cat ${MSX2_PAL_ROMS_DIR}/cx7mext.rom >> omega_msx2_pal.bin
+cat ${SYSTEM_ROMS_DIR}/nms8250_msx2sub.rom >> omega_msx2_pal.bin
 dd if=/dev/zero ibs=1k count=176 | tr "\000" "\377" >> omega_msx2_pal.bin
 
 # copy C-BIOS PAL ROMs
@@ -29,3 +28,18 @@ cat ${CBIOS_ROMS_DIR}/cbios_logo_msx2.rom >> omega_msx2_pal.bin
 dd if=/dev/zero ibs=1k count=16 | tr "\000" "\377" >> omega_msx2_pal.bin
 cat ${CBIOS_ROMS_DIR}/cbios_sub.rom >> omega_msx2_pal.bin
 dd if=/dev/zero ibs=1k count=176 | tr "\000" "\377" >> omega_msx2_pal.bin
+
+# copy MSX2+ NTSC ROMs
+cat ${SYSTEM_ROMS_DIR}/phc-35j_basic-bios2p.rom > omega_msx2+.bin
+dd if=/dev/zero ibs=1k count=32 | tr "\000" "\377" >> omega_msx2+.bin
+cat ${SYSTEM_ROMS_DIR}/phc-35j_msx2psub.rom >> omega_msx2+.bin
+cat ${SYSTEM_ROMS_DIR}/phc-35j_kanjibasic.rom >> omega_msx2+.bin
+dd if=/dev/zero ibs=1k count=144 | tr "\000" "\377" >> omega_msx2+.bin
+
+# copy C-BIOS NSX+ NTSC ROMs
+cat ${CBIOS_ROMS_DIR}/cbios_main_msx2+.rom >> omega_msx2+.bin
+cat ${CBIOS_ROMS_DIR}/cbios_logo_msx2+.rom >> omega_msx2+.bin
+dd if=/dev/zero ibs=1k count=16 | tr "\000" "\377" >> omega_msx2+.bin
+cat ${CBIOS_ROMS_DIR}/cbios_sub.rom >> omega_msx2+.bin
+dd if=/dev/zero ibs=1k count=176 | tr "\000" "\377" >> omega_msx2+.bin
+
